ecd0dea61fd47eda34b1cbddcd69227916975180
Ashley <avwede@gmail.com>
2021-03-08 15:10:54 -0500

    adding schema to backend

---
+++ backend/schema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const lyfeSchema = new Schema({
+    login: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    emergencyContacts:[
+        {
+            firstName: String,
+            lastName: String, 
+            relation: String, 
+            phoneNumber: String
+        }],
+    healthInfo:[
+        {
+            dateOfBirth: String,
+            allergies: [
+                {
+                    name: String, 
+                    schedule: [{day: String}]
+                }]
+        }],
+    classes:[
+        {
+            className: String,
+            schedule: [{day: String}]
+        }]
+    reminders:[
+        {
+            category: String,
+            schedule: [{day: Date}]
+        }]
+});
