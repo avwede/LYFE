e204df4cdb56454c989372c2042c33dd8b565324
Danny Vail <danny_vail@outlook.com>
2021-03-14 06:58:33 -0500

    DEFINE SERVER LEVEL MIDDLEWARE
    - Include url encoding and json
    - Move production static file links to server middleware

---
+++ packages/api/middleware/serverMiddleware.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const { NODE_ENV } = process.env;
+
+module.exports = {
+  serverMiddleware: (server) => {
+    server.use(express.urlencoded({ extended: true }));
+    server.use(express.json());
+    
+    // In production serve client build as static files.
+    if (NODE_ENV === 'production') {
+      server.use(express.static(path.resolve(__dirname, '../../client/build')));
+    }
+  },
+};
