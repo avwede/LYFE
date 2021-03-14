57b7c4e33ad973223d54d54e9b0752bd077c3166
Danny Vail <danny_vail@outlook.com>
2021-03-14 07:01:11 -0500

    DEFINE MAIN ROUTES FILE
    - Define route for api root
    - Move production redirects to main route file

---
+++ packages/api/routes/routes.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const path = require('path');
+const { NODE_ENV } = process.env;
+
+module.exports = {
+  routes: (server) => {
+    server.get('/api', (req, res) => {
+      res.set('Content-Type', 'application/json');
+      res.send('{"message": "LYFE API"}');
+    });
+
+    // In production redirect all other routes to the client home.
+    if (NODE_ENV === 'production') {
+      server.get('*', (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, '../../client/build', 'index.html')
+        );
+      });
+    } else {
+      server.get('*', (req, res) => {
+        res.set('Content-Type', 'application/json');
+        res.send(
+          '{"message": "In production this action will result in a redirect to the client."}'
+        );
+      });
+    }
+  },
+};
