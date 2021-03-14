3defb069f1369c41de556c1a9be5606331043f41
Danny Vail <danny_vail@outlook.com>
2021-03-13 21:48:02 -0600

    UPDATE SERVER TO RUN WITH EXPRESS
    - Run api server with express
    - Configure with environment variables
    - In production, redirect any attempts to reach a route not associated with api to client homepage

--- packages/api/server.js
+++ packages/api/server.js
@@ -1,14 +1,26 @@
-const http = require('http');
+require('dotenv').config();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const path = require('path');
+const server = express();
+const { PORT, NODE_ENV } = process.env;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// In production routes outside of the api are redirected to client.
+if (NODE_ENV == 'production') {
+  server.use(express.static('../client/build'));
+  server.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../client/build', 'index.html')
+    );
+    console.log(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+} else {
+  server.get('*', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message": "In production this action will result in a redirect to the client."}');
+  });
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, () => {
+  console.log(`\n*** Server listening on port ${PORT} ***\n`);
 });
