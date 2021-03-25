361280e1a9820bc23d5ccfc37eff16e17e72cb85
Danny Vail <danny_vail@outlook.com>
2021-03-25 05:28:27 -0500

    ADD SWAGGER ANNOTATIONS
    - Add Swagger annotations for error response components
    - Handle duplicate key error

--- packages/api/util/responses.js
+++ packages/api/util/responses.js
@@ -1,8 +1,8 @@
-const HTTP_STATUS_CODES = ['201', '400', '401', '403', '404', '500']
+const HTTP_STATUS_CODES = ['201', '400', '401', '403', '404', '500'];
 
 /**
  * Format and send an HTTP response.
- * 
+ *
  * @param {Object} res Express response object.
  * @param {Integer} status HTTP status code.
  * @param {Object} data JSON body.
@@ -13,8 +13,65 @@ const sendResponse = (res, status, data) => {
 };
 
 /**
- * Format and send an error HTTP response.
+ * @openapi
  * 
+ * components:
+ *  responses:
+ *    400BadRequest:
+ *      description: Bad Request.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *                example: "Validation failed: {Additional details}"
+ *    401Unauthorized:
+ *      description: Unauthorized.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *                example: Access denied. This request requires user authentication.
+ *    403Forbidden:
+ *      description: Forbidden.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *                example: This user is not authorized to perform this action.
+ *    404NotFound:
+ *      description: Not Found.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *                example: The document with the given ID does not exist.
+ *    500InternalServerError:
+ *      description: Internal server error.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *                example: This action could not be completed.
+ */
+
+/**
+ * Format and send an error HTTP response.
+ *
  * @param {Object} res Express response object.
  * @param {Object|String} err Error object or HTTP status code.
  * @param {String} message User friendly message to send on response object.
@@ -29,25 +86,23 @@ const sendError = (res, err, message) => {
 
 /**
  * Get and return the correct HTTP status code.
- * 
+ *
  * @param {Object|String} err Error object or HTTP status code.
  * @returns {Integer} The HTTP status code.
  */
 const getStatusCode = (err) => {
   let statusCode = 500;
 
-  if (err.name === 'ValidationError') statusCode = 400;
+  if (err.name === 'ValidationError' || err.code === 11000) statusCode = 400;
   if (err.name === 'CastError') statusCode = 404;
-  
-  if (HTTP_STATUS_CODES.includes(err))
-    statusCode = parseInt(err);
+  if (HTTP_STATUS_CODES.includes(err)) statusCode = parseInt(err);
 
   return statusCode;
 };
 
 /**
  * Construct JSON body for HTTP error response.
- * 
+ *
  * @param {Object|String} err Error object or HTTP status code.
  * @param {String} msg User friendly message to send on response object.
  * @returns {Object} JSON body.
@@ -55,12 +110,18 @@ const getStatusCode = (err) => {
 const getErrorJson = (err, msg) => {
   let errMsg = msg;
 
-  if (err.name === 'ValidationError')
-    errMsg = err.message;
-  
-  if (err.name === 'CastError')
-    errMsg = 'The document with the specified ID does not exist.';
+  if (err.name === 'ValidationError') errMsg = err.message;
 
+  if (err.name === 'CastError')
+    errMsg = 'The document with the given ID does not exist.';
+
+  if (err.code === 11000) {
+    errMsg = '';
+
+    for (let key in err.keyValue)
+      errMsg += `Duplicate Key: ${key} is already taken.`;
+  }
+    
   return { error: errMsg };
 };
 
