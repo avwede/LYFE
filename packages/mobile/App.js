372deb621bce923b44521822aade3bdbf2705cf7
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2021-03-28 14:27:52 -0400

    Added logo for mobile app.

--- packages/mobile/App.js
+++ packages/mobile/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source= {require('./assets/logo.png')} />
       <StatusBar style="auto" />
     </View>
   );
