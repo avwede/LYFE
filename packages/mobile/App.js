454ebe1f256ce408f5db7c80ecb1c3803c588563
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2021-03-30 20:34:00 -0400

    Added all mobile component files (currently copies of Register) and stack/tab navigators.

--- packages/mobile/App.js
+++ packages/mobile/App.js
@@ -1,17 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './src/screens/Login.js';
+import Register from './src/screens/Register.js';
+import ForgotPassword from './src/screens/ForgotPassword.js';
+import Dashboard from './src/screens/Dashboard.js';
+import Reminders from './src/screens/Reminders.js';
+import Health from './src/screens/Health.js';
+import School from './src/screens/School.js';
+import Profile from './src/screens/Profile.js';
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const isLoggedIn = 0;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source= {require('./assets/logo.png')} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {!isLoggedIn ?
+      (<Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      </Stack.Navigator>) :
+      (<Tab.Navigator initialRouteName="Dashboard">
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+        <Tab.Screen name="Reminders" component={Reminders} />
+        <Tab.Screen name="Health" component={Health} />
+        <Tab.Screen name="School" component={School} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>)}
+    </NavigationContainer>
   );
 }
+
+/*     <View style={styles.container}>
+<Image source= {require('./assets/logo.png')} />
+<StatusBar style="auto" />
+</View> **/
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ packages/mobile/src/screens/Dashboard.js
@@ -0,0 +1,137 @@
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Dimensions} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const {width: WIDTH} = Dimensions.get('window')
+class Dashboard extends Component{
+    constructor(props) {
+        super(props);
+    
+        this.focusNextField = this.focusNextField.bind(this);
+        this.inputs = {};
+      }
+    
+      focusNextField(id) {
+        this.inputs[id].focus();
+      }
+    render(){
+        return(
+            <LinearGradient colors={['#ACC1FF', '#9CECFF', '#DBF3FA']} style={styles.container}>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logo} source={require('../../assets/logo4.png')}></Image>
+                </View>
+                <View style={{alignItems:'center', marginTop:30}}>
+                    <TextInput
+                    style={styles.inputView}
+                    placeholder= 'First Name'
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                    onSubmitEditing = {() => this.focusNextField('two')}
+                    ref={ input => {this.inputs['one'] = input;}}
+                    ></TextInput>
+                </View>
+                <View style={{alignItems:'center'}}>
+                    <TextInput
+                    style={styles.inputView}
+                    placeholder= 'Last Name'
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                    onSubmitEditing = {() => this.focusNextField('three')}
+                    ref={ input => {this.inputs['two'] = input;}}
+                    ></TextInput>
+                </View>
+                <View style={{alignItems:'center'}}>
+                    <TextInput
+                    style={styles.inputView}
+                    placeholder= 'Email'
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                    onSubmitEditing = {() => this.focusNextField('four')}
+                    ref={ input => {this.inputs['three'] = input;}}
+                    ></TextInput>
+                </View>
+                <View style={{alignItems:'center'}}>
+                    <TextInput
+                    style={styles.inputView}
+                    placeholder= 'Password'
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    secureTextEntry={true}
+                    blurOnSubmit={true}
+                    ref={ input => {this.inputs['four'] = input;}}
+                    ></TextInput>
+                </View>
+                <TouchableOpacity style={styles.loginBtn}>
+                    <Text style={styles.signUp} >Create Account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.toLogin}>
+                <View style={{alignContent:'center', marginTop:30}}>
+                    <Text style={styles.loginText} >
+                        Already have an account? Tap here.
+                    </Text>
+                </View>
+                </TouchableOpacity>
+            </LinearGradient>
+        );
+    }
+}
+export default Dashboard;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    logoContainer: {
+        alignItems:'center',
+        paddingBottom:40,
+        marginTop:70
+    },
+    logo:{
+        width: 140,
+        height: 140,
+    },
+    inputContainer:{
+        marginTop:10
+    },
+    inputView:{
+        width: WIDTH - 100,
+        backgroundColor:"white",
+        borderRadius:25,
+        height:50,
+        marginBottom:20,
+        justifyContent:'center',
+        paddingLeft:20,
+        paddingBottom: 3,
+      },
+    loginBtn:{
+        width: 300,
+        backgroundColor:"#181414",
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop: 10,
+    },
+    toLogin:{
+        alignContent: 'center',
+        marginTop:60
+    },
+    signUp:{
+        color:'white',
+        fontSize: 13,
+        fontFamily: 'sans-serif',
+    },
+    loginText:{
+        color:'black',
+        fontSize: 12,
+        fontFamily: 'sans-serif',
+    }
+});
