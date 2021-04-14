9da3856f89d3558e6753e97f3b44b2f347e4f6d0
Danny Vail <danny_vail@outlook.com>
2021-04-14 10:52:27 -0500

    IMPLEMENT REMINDER TYPES ROUTES
    - Define GET /api/reminder-types to get all reminder typers
    - Define GET /api/reminder-types/:id to get reminder type by id
    - Define POST /api/reminder-types to create a reminder type
    - Define PUT /api/reminder-types/:id to update a reminder type
    - Define DELETE /api/reminder-types/:id to delete a reminder type

---
+++ packages/api/routes/reminderTypes.js
@@ -0,0 +1,351 @@
+const router = require('express').Router();
+const ReminderType = require('../models/ReminderType');
+const User = require('../models/User');
+const { sendResponse, sendError } = require('../util/responses');
+const { authenticateJWT } = require('../middleware/routerMiddleware');
+
+/**
+ * @openapi
+ * 
+ * paths:
+ *  /api/reminder-types:
+ *    get:
+ *      security:
+ *        - BearerAuth: []
+ *      tags: [reminders]
+ *      summary: Get reminder types.
+ *      description:  Get all reminder types for the user contained in the required JSON Web Token. 
+ *        This action can only be performed by an authenticated user.
+ *      operationId: getReminderTypes
+ *      responses:
+ *        200:
+ *          description: List of reminder types successfully retrieved.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/ReminderType'
+ *        401:
+ *          $ref: '#/components/responses/401Unauthorized'
+ *        500:
+ *          $ref: '#/components/responses/500InternalServerError'
+ */
+router.get('/', authenticateJWT, (req, res) => {
+  const userId = req.tokenPayload.id;
+
+  ReminderType.find({ user: userId })
+    .then(results => {
+      sendResponse(res, 200, results);
+    })
+    .catch(err => {
+      console.log(err);
+      sendError(res, err, err.message);
+    });
+});
+
+/**
+ * @openapi
+ * 
+ * paths:
+ *  /api/reminder-types/{reminderTypeId}:
+ *    get:
+ *      security:
+ *        - BearerAuth: []
+ *      tags: [reminders]
+ *      summary: Get reminder type by id.
+ *      description:  Get a single reminder type for the user contained in the required JSON Web Token.
+ *        This action can only be performed by an authenticated user.
+ *      operationId: getReminderType
+ *      parameters:
+ *        - in: path
+ *          name: reminderTypeId
+ *          required: true
+ *          description: The Object Id of the reminder type to be retrieved.
+ *          schema:
+ *            type: string
+ *            format: objectId
+ *          example: 6058319d6aedde248dbad720
+ *      responses:
+ *        200:
+ *          description: Reminder Type successfully retrieved.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/ReminderType'
+ *        401:
+ *          $ref: '#/components/responses/401Unauthorized'
+ *        403:
+ *          $ref: '#/components/responses/403Forbidden'
+ *        404:
+ *          description: Not found.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  error: 
+ *                    type: string
+ *                    example: Reminder Type with id does not exist.
+ *        500:
+ *          $ref: '#/components/responses/500InternalServerError'
+ */
+router.get('/:id', authenticateJWT, async (req, res) => {
+  const { id } = req.params;
+  const userId = req.tokenPayload.id;
+  const reminderType = await ReminderType.findById(id);
+
+  if (reminderType) {
+    if (reminderType.user == userId)
+      sendResponse(res, 200, reminderType);
+    else
+      sendError(res, 403, 'This user is not authorized to perform this action.');
+  } else {
+    sendError(res, 404, `Reminder Type with id ${id} does not exist.`);
+  }
+});
+
+/**
+ * @openapi
+ * 
+ * paths:
+ *  /api/reminder-types:
+ *    post:
+ *      security:
+ *        - BearerAuth: []
+ *      tags: [reminders]
+ *      summary: Create reminder type.
+ *      description: Create a new reminder type for the user contained in the required JSON Web Token. 
+ *        This action can only be performed by an authenticated user.
+ *      operationId: createReminderType
+ *      requestBody:
+ *        description: ReminderType
+ *        required: true
+ *        content:
+ *          application/json:
+ *            title: Reminder Type
+ *            type: object
+ *            required:
+ *              - type
+ *            properties:
+ *              type:
+ *                type: string
+ *                example: Class
+ *              color:
+ *                type: string
+ *                example: '#001C5C'
+ *      responses:
+ *        201:
+ *          description: New reminder type successfully created.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Reminder'
+ *        400:
+ *          $ref: '#/components/responses/400BadRequest'
+ *        401:
+ *          $ref: '#/components/responses/401Unauthorized'
+ *        500:
+ *          $ref: '#/components/responses/500InternalServerError'
+ */
+router.post('/', authenticateJWT, async (req, res) => {
+  const userId = req.tokenPayload.id;
+  const newReminderType = req.body;
+
+  newReminderType['user'] = userId;
+
+  ReminderType.create(newReminderType)
+    .then(reminderType => {
+      sendResponse(res, 201, reminderType);
+    })
+    .catch(err => {
+      sendError(res, err, 'The reminder type could not be created.');
+    });
+});
+
+/**
+ * @openapi
+ * 
+ * paths:
+ *  /api/reminder-types/{reminderTypeId}:
+ *    put:
+ *      security:
+ *        - BearerAuth: []
+ *      tags: [reminders]
+ *      summary: Update a reminder type.
+ *      description: Update a reminder type for the user contained in the required JSON Web Token. 
+ *        This action can only be performed by an authenticated user.
+ *      operationId: updateReminderType
+ *      parameters:
+ *        - in: path
+ *          name: reminderTypeId
+ *          required: true
+ *          description: The Object Id of the reminder type to be updated.
+ *          schema:
+ *            type: string
+ *            format: objectId
+ *          example: 6058319d6aedde248dbad720
+ *      requestBody:
+ *        description: ReminderType
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ReminderType'
+ *      responses:
+ *        200:
+ *          description: Reminder type successfully updated.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/ReminderType'
+ *        400:
+ *          $ref: '#/components/responses/400BadRequest'
+ *        401:
+ *          $ref: '#/components/responses/401Unauthorized'
+ *        403:
+ *          $ref: '#/components/responses/403Forbidden'
+ *        404:
+ *          description: Not found.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  error: 
+ *                    type: string
+ *                    example: Reminder Type with id does not exist.
+ *        500:
+ *          $ref: '#/components/responses/500InternalServerError'
+ */
+router.put('/:id', authenticateJWT, async (req, res) => {
+  const { id } = req.params;
+  const updatedFields = req.body;
+  const userId = req.tokenPayload.id;
+  const reminderType = await ReminderType.findById(id);
+
+  if (reminderType) {
+    if (reminderType.user == userId) {
+      const updatedReminderType = updateReminderTypeFields(reminderType, updatedFields);
+      save(reminderType, res, updatedReminderType);
+    }
+    else {
+      sendError(res, 403, 'This user is not authorized to perform this action.');
+    }
+  } else {
+    sendError(res, 404, `Reminder Type with id ${id} does not exist.`);
+  }
+});
+
+/**
+ * @openapi
+ * 
+ * paths:
+ *  /api/reminder-types/{reminderTypeId}:
+ *    delete:
+ *      security:
+ *        - BearerAuth: []
+ *      tags: [reminders]
+ *      summary: Delete a reminder type.
+ *      description: Delete a reminder type by id for the user contained in the required JSON Web 
+ *        Token. This action can only be performed by an authenticated user.
+ *      operationId: deleteReminder
+ *      parameters:
+ *        - in: path
+ *          name: reminderTypeId
+ *          required: true
+ *          description: The Object Id of the reminder type to be deleted.
+ *          schema:
+ *            type: string
+ *            format: objectId
+ *          example: 6058319d6aedde248dbad720
+ *      responses:
+ *        204:
+ *          description: Reminder type successfully deleted.
+ *        401:
+ *          $ref: '#/components/responses/401Unauthorized'
+ *        403:
+ *          $ref: '#/components/responses/403Forbidden'
+ *        404:
+ *          description: Not found.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  error: 
+ *                    type: string
+ *                    example: Reminder Type with id does not exist.
+ *        500:
+ *          $ref: '#/components/responses/500InternalServerError'
+ */
+router.delete('/:id', authenticateJWT, async (req, res) => {
+  const { id } = req.params;
+  const userId = req.tokenPayload.id;
+  const reminderType = await ReminderType.findById(id);
+
+  if (reminderType) {
+    if (reminderType.user == userId) {
+      deleteReminderType(id, res);
+    } else {
+      sendError(res, 403, 'This user is not authorized to perform this action.');
+    }
+  } else {
+    sendError(res, 404, `Reminder Type with id ${id} does not exist.`);
+  }
+});
+
+/**
+ * Delete the remainder type with the given id.
+ * 
+ * @param {String} id Reminder type object id.
+ * @param {Object} resp Express response object.
+ */
+const deleteReminderType = (id, res) => {
+  ReminderType.findByIdAndRemove({ _id: id })
+    .then(() => {
+      sendResponse(res, 204, {});
+    })
+    .catch(err => {
+      sendError(res, err, `The reminder type with id ${id} could not be removed.`);
+    });
+};
+
+/**
+ * Save the document and send an OK response. If the document does not validate, abort and send
+ * error HTTP response.
+ * 
+ * @param {Document} doc Mongoose document.
+ * @param {Object} res Express response object.
+ * @param {Object} respBody Response body for the OK HTTP response.
+ */
+const save = (doc, res, respBody) => {
+  doc.save()
+    .then(() => {
+      sendResponse(res, 200, respBody);
+    })
+    .catch(err => {
+      sendError(res, err, err.message);
+    });
+};
+
+/**
+ * Use the update fields to perform a partial update on the given reminder type subdocument.
+ * 
+ * @param {Subdocument} reminderType Mongoose reminder type subdocument.
+ * @param {Object} updatedFields The collection of updated fields for this reminder type.
+ * @returns {Subdocument} The updated reminder type subdocument.
+ */
+const updateReminderTypeFields = (reminderType, updatedFields) => {
+  Object.keys(updatedFields).forEach((key) => {
+    reminderType[key] = updatedFields[key];
+  });
+
+  return reminderType;
+};
+
+module.exports = {
+  reminderTypeRouter: router,
+};
