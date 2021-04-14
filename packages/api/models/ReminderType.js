d81672fce70e18575d88d156d67e078d722cd592
Danny Vail <danny_vail@outlook.com>
2021-04-14 10:47:01 -0500

    UPDATE REMINDER MODELS
    - Model schema for reoccuring reminders
    - Add Swagger component schema to reminder type

--- packages/api/models/ReminderType.js
+++ packages/api/models/ReminderType.js
@@ -1,11 +1,39 @@
 const mongoose = require('mongoose');
 
+/**
+ * @openapi
+ * 
+ * components:
+ *  schemas:
+ *    ReminderType:
+ *      title: ReminderType
+ *      type: object
+ *      required:
+ *        - type
+ *        - user
+ *      properties:
+ *        type:
+ *          type: string
+ *          example: Class
+ *        color:
+ *          type: string
+ *          example: '#001C5C'
+ *        user:
+ *          type: string
+ *          format: objectId
+ *          example: 5b1ed13e8cea93c6ba72b1da
+ */
 const reminderTypeSchema = new mongoose.Schema({
   type: {
     type: String,
     required: [true, 'Type is required for Reminder Type.'],
   },
   color: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User Id is required for Reminder Type.'],
+  },
 });
 
 module.exports = mongoose.model('ReminderType', reminderTypeSchema);
